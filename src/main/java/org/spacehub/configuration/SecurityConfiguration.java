9e3fd3af7a01d2e5d0c5b6f6c4a7c694f2254a8c
somiljain2006 <somil16022006@gmail.com>
2025-10-14 22:28:43 +0530

    Updated cors config

--- src/main/java/org/spacehub/configuration/SecurityConfiguration.java
+++ src/main/java/org/spacehub/configuration/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8080"));
+    config.setAllowedOrigins(List.of("https://space-hub-frontend.vercel.app/"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
     config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin",
             "X-Requested-With"));
