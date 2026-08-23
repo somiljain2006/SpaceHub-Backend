686c7abab28d778f29fb0a550c6c7a51b2694ae2
somil jain <somil16022006@gmail.com>
2026-08-24 01:11:13 +0530

    Update HomepageTests.java

--- src/test/java/org/spacehub/HomepageTests.java
+++ src/test/java/org/spacehub/HomepageTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class HomepageTests {
-// testing in real life
+// testing in real lifee
   @Test
   void contextLoads() {
   }
