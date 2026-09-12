b7d43bec743816042b145d7a5358f12e57c092c4
somil jain <somil16022006@gmail.com>
2026-09-12 22:57:00 +0530

    Update HomepageTests.java

--- src/test/java/org/spacehub/HomepageTests.java
+++ src/test/java/org/spacehub/HomepageTests.java
@@ -1,5 +1,12 @@
 package org.spacehub;
 
+/**
+ * HomepageTests.java
+ *
+ * Basic test class for verifying that the Spring Boot application context loads correctly
+ * in the "test" profile for the SpaceHub application.
+ */
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -7,7 +14,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class HomepageTests {
-// testing in real lifee
+  // testing in real lifee
   @Test
   void contextLoads() {
   }
